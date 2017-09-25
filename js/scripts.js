f7ad7f545801ff3fff2dac0741a3b28c165c6cf5
Juan Mezquita <jumezq@gmail.com>
2017-09-25 23:21:25 +0200

    Starting Jquery UI

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
 $(function() {
-	
+
+    $( "#tabs-nohdr" ).tabs();
+  
+	/*$( document ).click(function() {
+  $( "#outer" ).toggle( "explode" );
+  $( "#outer2" ).toggle( "explode" );
+});
+*/
+              /*$("#outer").toggle(
+                function() {
+                    $("#outer").hide('explode', {}, 600);
+                },
+                function() {
+                    $("#outer").show('explode', {}, 600);
+                }
+             );*/
+
+
+   
 }); 
