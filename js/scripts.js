3f5db33598fef100f11dcbc33fa5a4f2acdc7e13
DiegoSerra <30292837+DiegoSerra@users.noreply.github.com>
2017-10-24 09:13:28 +0200

    fade in on scroll

--- js/scripts.js
+++ js/scripts.js
@@ -66,6 +66,21 @@ $(function() {
           $(this).parent().siblings().removeClass('active');
         }
       })
+
+      /* Check the location of each desired element */
+      $('section').each( function(i){
+        
+        var bottom_of_object = $(this).offset().top + $(this).outerHeight() - 250;
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+        
+        /* If the object is completely visible in the window, fade it it */
+        if( bottom_of_window > bottom_of_object ){
+            
+            $(this).animate({'opacity':'1'},500);
+                
+        }
+        
+    });
       
     })
   
